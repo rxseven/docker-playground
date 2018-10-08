21613e5515e507e79a9db8383890d1dee7ae6c2f
Theerawat Pongsupawat <rxseven.com@gmail.com>
2018-10-08 19:06:20 +0700

    Refactor file & folder structure and update image tag

--- scripts/deploy.image.sh
+++ scripts/deploy.image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Build a production image
+# Build a production image for deployment
 docker-compose -f docker-compose.yml -f docker-compose.production.yml build
 
 # Login to Docker Hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-# Push a production image to Docker Hub
-docker push rxseven/playground:0.0.1
+# Push the production image to Docker Hub
+docker push rxseven/playground:0.0.2
