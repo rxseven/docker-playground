34505d0e33ebe6fc838653dfc638839d11f9dc6d
Theerawat Pongsupawat <rxseven.com@gmail.com>
2018-10-11 14:55:17 +0700

    Specify zip file for deploying to AWS

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => (
           <Route component={NotFound} />
         </Switch>
       </main>
-      <footer>Footer</footer>
+      <footer>Footer - v0.0.3</footer>
     </React.Fragment>
   </Router>
 );
