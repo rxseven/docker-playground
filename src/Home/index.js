aa5b2ff9e2f6147974a7bf585bc34de3467213ce
Theerawat Pongsupawat <rxseven.com@gmail.com>
2018-10-17 15:43:30 +0700

    Update Home page

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => (
   <div>
-    <h1>Home page | Docker Playground</h1>
+    <h1>Home page | Docker Playground with Proxy</h1>
     <p>
       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industry's standard dummy text ever since the
