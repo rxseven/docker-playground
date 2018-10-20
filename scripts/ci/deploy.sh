83fa7a27d05d5cb4b09e9bc7d2b195df73b9a6b1
Theerawat Pongsupawat <rxseven.com@gmail.com>
2018-10-20 13:31:12 +0700

    Fix CI_ env

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Create deployment configuration
-# sed -i='' "s/<IMAGE_ACCOUNT>/${CI_BUILD_ACCOUNT}/" ${CI_PRODUCTION_CONFIG}
-# sed -i='' "s/<IMAGE_REPO>/${CI_BUILD_REPO}/" ${CI_PRODUCTION_CONFIG}
-# sed -i='' "s/<IMAGE_TAG>/${CI_BUILD_VERSION}/" ${CI_PRODUCTION_CONFIG}
-sed -ie 's|\(.*"Name"\): "\(.*\)",.*|\1: '"\"${CI_BUILD_ACCOUNT}\/${CI_BUILD_REPO}:${CI_BUILD_VERSION}\",|" ${CI_PRODUCTION_CONFIG}
-zip ${CI_BUILD_ZIP} ${CI_PRODUCTION_CONFIG}
+# sed -i='' "s/<IMAGE_ACCOUNT>/${BUILD_ACCOUNT}/" ${PRODUCTION_CONFIG}
+# sed -i='' "s/<IMAGE_REPO>/${BUILD_REPO}/" ${PRODUCTION_CONFIG}
+# sed -i='' "s/<IMAGE_TAG>/${BUILD_VERSION}/" ${PRODUCTION_CONFIG}
+sed -ie 's|\(.*"Name"\): "\(.*\)",.*|\1: '"\"${BUILD_ACCOUNT}\/${BUILD_REPO}:${BUILD_VERSION}\",|" ${PRODUCTION_CONFIG}
+zip ${BUILD_ZIP} ${PRODUCTION_CONFIG}
 
 # Build a production image for deployment
 docker-compose -f docker-compose.yml -f docker-compose.production.yml build app
@@ -14,4 +14,4 @@ docker-compose -f docker-compose.yml -f docker-compose.production.yml build app
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
 # Push the production image to Docker Hub
-docker push rxseven/playground:${CI_BUILD_VERSION}
+docker push rxseven/playground:${BUILD_VERSION}
