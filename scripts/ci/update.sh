9aa9f3a42abcae57c1ed85d2a709956fa0a1cdee
Theerawat Pongsupawat <rxseven.com@gmail.com>
2018-10-21 14:39:49 +0700

    Remove CI bash scripts

--- scripts/ci/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Update Docker Compose
-sudo rm ${BINARY_PATH}/docker-compose
-curl -L ${DOCKER_COMPOSE_REPO}/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
-chmod +x docker-compose
-sudo mv docker-compose ${BINARY_PATH}
