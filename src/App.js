fa3b559e5302e581dbae71ac6bfee7ddd55f0b16
ME (MBP) <rxseven.com@gmail.com>
2018-11-08 20:04:27 +0700

    Update release version to v0.0.27

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => (
           <Route component={NotFound} />
         </Switch>
       </main>
-      <footer>Footer - v0.0.26 | Docker Playground</footer>
+      <footer>Footer - v0.0.27 | Docker Playground</footer>
     </React.Fragment>
   </Router>
 );
